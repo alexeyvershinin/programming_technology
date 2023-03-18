e053cfb102dea7034dc90562f9ce0d3ef367cbc2
alexeyvershinin <115144824+alexeyvershinin@users.noreply.github.com>
2023-03-18 14:12:59 +0300

    Update task_1.cpp
    Доработал код, чтобы при выводе двузначных и т.д. чисел между ними выводился пробел

--- homework_4.2/task_1.cpp
+++ homework_4.2/task_1.cpp
@@ -7,7 +7,10 @@ int main() {
         printf("Enter the value of the array element:");
         scanf("%i", &mas[i]);
     }
+    // для лучшей читаемости добавил символ переноса строки
+    printf("\n");
     for (int i=0; i<7; i++) {
-        printf("%2i", mas[i]);
+//        printf("%2i", mas[i]);
+        printf("%i ", mas[i]);
     }
 }
